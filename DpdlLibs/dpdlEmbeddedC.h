fc185b3ad1b18aa75f9bb1910e232168c9e99c8f
Armin Costa <armincost@gmail.com>
2023-09-28 21:40:54 +0200

    add example

---
+++ DpdlLibs/dpdlEmbeddedC.h
@@ -0,0 +1,166 @@
+# File: dpdlEmbeddedC.h
+#
+# Example: This script executes embedded ANSI C code
+#
+# (c)opyright 2009
+# SEE Solutions
+#
+# e-mail: info@seesolutions.it
+#
+#
+include("dpdllib.h")
+
+func testEmbeddedC(int n, double x, string a)
+	println("pushing vars to dpdl stack: " + n + " " + x + " " + a)
+	
+	dpdl_stack_push(n, x, a)
+	
+	println("executing interpreted C code...")
+	println("-------------------------------")
+	
+	>>picoc
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <string.h>
+	#include <time.h>
+	#include <unistd.h>
+	#include <math.h>
+	
+	typedef int u32;
+	
+	struct data
+	{
+    	int id;
+    	int node;
+	};
+
+	enum keys
+	{
+    	a = 3,
+    	b = 6,
+    	c = 9,
+    	d,
+    	e = 11,
+    	f = 23,
+    	g,
+    	h
+	};
+
+	struct data mydata;
+	enum keys mykeys;
+	
+	int testEmbeddedC(double x, int n, int *a) {
+	    int i;
+	
+	    printf("x = %f\n", x);
+	    for(i=0; i<n; i++){
+	       printf("a[%d] = %d\n", i, a[i]);
+	       a[i] = i + 100;
+	    }
+	    a[0] = 100;
+	    return 0;
+	}
+	
+	/* main entry */
+	int main(int argc, char **argv)
+	{
+	 	printf("********************************\n");
+		printf("hello embedded C from Dpdl\n\n");
+		
+		time_t time_start;
+		time(&time_start);
+		
+		u32 myint = 3;
+		
+		mydata.id = 23;
+		mydata.node = 369;
+		
+		printf("id: %d\n", mydata.id);
+		printf("node: %d\n", mydata.node);
+		printf("keys: %d %d %d %d %d %d %d %d\n", a, b, c, d, e, f, g, h);
+		printf("myint: %d\n", myint);
+		
+		struct data *ptr_data = &mydata;
+
+		printf("ptr_data->id = %d\n", ptr_data->id);
+		printf("ptr_data->node = %d\n\n", ptr_data->node);
+		
+	    int cnt;
+	
+	    printf("num params: %d\n", argc);
+	    for (cnt = 0; cnt < argc; cnt++){
+	        printf("	param %d: %s\n", cnt, argv[cnt]);
+	    }
+	    
+	    int n;
+	    double x;
+	    char param[256];
+	    sscanf(argv[0], "%d", &n);
+		sscanf(argv[1], "%lf", &x);
+		sscanf(argv[2], "%s", &param);
+		
+		printf("myparam: %s\n", &param); 
+		int mynum[6] = {1, 2, 3, 4, 5, 6};
+		
+		testEmbeddedC(x, n, &mynum);
+		x = 23.0;
+		testEmbeddedC(x, n, &mynum);
+		printf("\n");
+		
+		goto math_operations;
+		
+ 		{
+        	int b;
+        	
+			math_operations:
+			printf("Math operations:\n\n");
+			printf("%f\n", sin(0.12));
+			printf("%f\n", cos(0.12));
+			printf("%f\n", tan(0.12));
+			printf("%f\n", asin(0.12));
+			printf("%f\n", log(0.12));
+			printf("%f\n", pow(0.12, 0.12));
+			printf("%f\n", sqrt(0.12));
+			printf("%f\n", round(12.34));
+			printf("%f\n", ceil(12.34));
+			printf("%f\n", floor(12.34));    
+			printf("\n");
+			
+			goto finish;
+    	}
+    	
+   	finish:
+    	
+    	sleep(1);
+    	time_t time_end;
+    	time(&time_end);
+    	
+    	double exec_time = difftime(time_end, time_start);
+    	printf("total exec_time (sec): %f\n", exec_time);
+	    	    
+	    return 0;
+	}
+	<<
+	println("-------------------------------")
+	
+	int exit_code = dpdl_exit_code()
+	println("finished executing embedded C code: " + exit_code)
+	
+end
+
+
+# main
+println("testing embedded interpreted C code from Dpdl")
+
+string a = "a1 a2 a3 a4 a5 a6"
+int n = 6
+double x = 10.0
+
+testEmbeddedC(n, x, a)
+
+>>picoc
+	#include <stdio.h>
+	
+	printf("Finished!!!\n");
+<<
+
---
+++ DpdlLibs/dpdlSimpleC.h
@@ -0,0 +1,45 @@
+# File: dpdlSimpleC.h
+#
+# Example: This script executes embedded ANSI C code 
+#
+# (c)opyright 2009
+# SEE Solutions
+#
+# e-mail: info@seesolutions.it
+#
+#
+
+# main
+# starting with Dpdl, pushing parameters on the stack and embedding C
+
+println("testing embedded C code in Dpdl")
+
+int n = 6
+double x = 10.0
+string a = "test"
+
+dpdl_stack_push(n, x, a)
+
+>>picoc
+	#include <stdio.h>
+	
+	int main(int argc, char **argv){
+		printf("Hello C from Dpdl!\n");
+		printf("\n");
+		printf("num params: %d\n", argc);
+		int cnt;
+	    for (cnt = 0; cnt < argc; cnt++){
+	        printf("	param %d: %s\n", cnt, argv[cnt]);
+	    }
+	    return 0;
+	}
+<<
+int exit_code = dpdl_exit_code()
+
+println("ebedded C exit code: " + exit_code);
+
+# again Dpdl...
+
+object str = loadObj("String", "Dpdl embedded C")
+bool b = str.contains("C")
+println("Dpdl contains C: " + b)
